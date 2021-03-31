22375e8603ec518354586b11a1ef3dafe528031e
Ramesh Pokhrel <kanxoramesh@gmail.com>
2021-03-31 21:15:29 +0545

    manage counter

---
+++ src/counter/RenderOptimize.js
@@ -0,0 +1,71 @@
+import React, {
+  createContext,
+  useContext,
+  useReducer,
+  useRef,
+  useEffect,
+} from 'react'
+import { createContainer } from 'react-tracked'
+
+const useFlasher = () => {
+  const ref = useRef(null)
+  useEffect(() => {
+    ref.current.classList.add('flash')
+    setTimeout(() => {
+      ref.current.classList.remove('flash')
+    }, 300)
+  })
+  return ref
+}
+
+const initialState = {
+  count1: 0,
+  count2: 0,
+}
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return {
+        ...state,
+        [action.name]: state[action.name] + 1,
+      }
+    case 'DECREMENT':
+      return {
+        ...state,
+        [action.name]: state[action.name] - 1,
+      }
+    default:
+      return state
+  }
+}
+
+const useValue = () => useReducer(reducer, initialState)
+
+const {
+  useTracked: useGlobalState,
+  Provider: GlobalStateProvider,
+} = createContainer(useValue)
+
+const Counter = ({ name }) => {
+  const [state, dispatch] = useGlobalState()
+  console.log(name)
+  return (
+    <div ref={useFlasher()}>
+      {state[name]}
+      <button onClick={() => dispatch({ type: 'INCREMENT', name })}>+1</button>
+      <button onClick={() => dispatch({ type: 'DECREMENT', name })}>-1</button>
+    </div>
+  )
+}
+
+const App = () => (
+  <GlobalStateProvider>
+    <h1>Count1</h1>
+    <Counter name='count1' />
+    <h1>Count2</h1>
+    <Counter name='count2' />
+  </GlobalStateProvider>
+)
+
+export default App
--- src/index.js
+++ src/index.js
@@ -7,11 +7,12 @@ import * as serviceWorker from './serviceWorker'
 import App from './components/App'
 import UseStateApp from './counter/WithUseState'
 import UseReducerApp from './counter/WithUseReducer'
+import RenderOptimize from './counter/RenderOptimize'
 import Firebase, { FirebaseContext } from './components/Firebase'
 
 ReactDOM.render(
   <FirebaseContext.Provider value={new Firebase()}>
-    <UseReducerApp />
+    <RenderOptimize />
   </FirebaseContext.Provider>,
   document.getElementById('root')
 )
