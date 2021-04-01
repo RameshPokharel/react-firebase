8ec028d69ec44a06d06a083868b26c653cefc00a
Ramesh Pokhrel <kanxoramesh@gmail.com>
2021-04-01 13:54:23 +0545

    new todo with immer

---
+++ src/newTodo/TodoItem.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+import { useQuery } from './hooks/useQuery'
+import { useDeleteTodo } from './hooks/useDeleteTodo'
+import { useToggleTodo } from './hooks/useToggleTodo'
+import { useFlasher } from '../todo/utils'
+
+const renderHighlight = (title, query) => {
+  if (!query) return title
+  const index = title.indexOf(query)
+  if (index === -1) return title
+  return (
+    <React.Fragment>
+      {title.slice(0, index)}
+      <b>{query}</b>
+      {title.slice(index + query.length)}
+    </React.Fragment>
+  )
+}
+
+const TodoItem = ({ id, title, completed }) => {
+  const { getQuery } = useQuery()
+  const deleteTodo = useDeleteTodo()
+  const toggleTodo = useToggleTodo()
+  return (
+    <li ref={useFlasher()}>
+      <input
+        type='checkbox'
+        checked={!!completed}
+        onChange={() => toggleTodo(id)}
+      />
+      <span
+        style={{
+          textDecoration: completed ? 'line-through' : 'none',
+        }}
+      >
+        {completed ? title : renderHighlight(title, getQuery())}
+      </span>
+      <button onClick={() => deleteTodo(id)}>Delete</button>
+    </li>
+  )
+}
+
+export default React.memo(TodoItem)
--- src/todo/App.js
+++ src/todo/App.js
@@ -3,10 +3,13 @@ import React from 'react'
 import { Provider } from './store'
 import TodoList from './TodoList'
 
+import { NewProvider } from '../newTodo/store'
+import NewTodoList from '../newTodo/TodoList'
+
 const App = () => (
-  <Provider>
-    <TodoList />
-  </Provider>
+  <NewProvider>
+    <NewTodoList />
+  </NewProvider>
 )
 
 export default App
